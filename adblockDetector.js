249905f3e56f83b08c87db25ef5d6ff6a99331c4
akunewb <83855171+akunewb@users.noreply.github.com>
2022-09-01 01:17:24 +0700

    Add files via upload

---
+++ adblockDetector.js
@@ -0,0 +1,19 @@
+//<![CDATA[
+
+(function adBlockInfo() {
+    var _0x5da8x2 = document['createElement']('script');
+    _0x5da8x2['type'] = 'text/javascript';
+    _0x5da8x2['src'] = '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+    _0x5da8x2['async'] = true;
+    _0x5da8x2['onerror'] = function() {
+        var _0x5da8x3 = document['createElement']('div');
+        _0x5da8x3['id'] = 'adBlockInfo';
+        _0x5da8x3['innerHTML'] = '<div class="adBlockTxt"><span class="adBlockTitle">' + titleAd + '</span><br/><svg viewBox="0 0 24 24"><path d="M13,13H11V7H13M12,17.3A1.3,1.3 0 0,1 10.7,16A1.3,1.3 0 0,1 12,14.7A1.3,1.3 0 0,1 13.3,16A1.3,1.3 0 0,1 12,17.3M15.73,3H8.27L3,8.27V15.73L8.27,21H15.73L21,15.73V8.27L15.73,3Z"></path></svg><br/>' + notifAd + '</div>';
+        document['body']['append'](_0x5da8x3);
+        document['body']['style']['overflow'] = 'hidden';
+        window['adblock'] = true
+    };
+    var _0x5da8x4 = document['getElementsByTagName']('script')[0];
+    _0x5da8x4['parentNode']['insertBefore'](_0x5da8x2, _0x5da8x4)
+})()
+//]]>
